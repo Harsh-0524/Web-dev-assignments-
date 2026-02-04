1f092d78440b2856cf699c3afb76d2a86d842249
Harsh Kumar <harshkumar050624@gmail.com>
2026-02-04 10:50:47 +0530

    new classes added

---
+++ CLASS 9/mini exercise/notes.md
@@ -0,0 +1,9 @@
+1. CREATE DUMMY UI
+   a. box-model
+   b.HTML
+   c.CSS
+
+2.JS
+  a.Listening for input
+  b. business logic
+  c.update dom   
---
+++ CLASS 9/script.js
@@ -0,0 +1,9 @@
+document.querySelector("#grandparent").addEventListener("click",()=>{
+    console.log("grandparent clicked!")
+})
+document.querySelector("#parent").addEventListener("click",()=>{
+    console.log("parent clicked!")
+})
+document.querySelector("#child").addEventListener("click",()=>{
+    console.log("child clicked!")
+})
